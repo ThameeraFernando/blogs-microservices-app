5aa4642541870649863c055affa9d9f88170e409
ThameeraFernando <sithnuwan111@gmail.com>
2023-12-26 11:21:54 +0530

    add comments

---
+++ comments/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const app = express();
+const PORT = 5000;
+
+const commentsByPostId = {};
+
+app.use(bodyParser.json());
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to port ${PORT}`);
+});
