9d440207e8d9e241aec3f528ab13bbf433c8df18
ThameeraFernando <sithnuwan111@gmail.com>
2023-12-26 15:24:47 +0530

    add events post reqs

--- EventBus/index.js
+++ EventBus/index.js
@@ -10,15 +10,15 @@ app.use(cors());
 const PORT = 7000;
 
 app.post("/events", async (req, res) => {
-  try {
-    const event = re.body;
-    await axios.post("http://localhost:5000/events", event);
-    await axios.post("http://localhost:4000/events", event);
-    await axios.post("http://localhost:6000/events", event);
-    res.send({ status: "OK" });
-  } catch (error) {
-    res.send({ status: "Failed" });
-  }
+  //   try {
+  const event = req.body;
+  await axios.post("http://localhost:5000/events", event);
+  await axios.post("http://localhost:4000/events", event);
+  //   await axios.post("http://localhost:6000/events", event);
+  res.send({ status: "OK" });
+  //   } catch (error) {
+  //     res.send({ status: "Failed" });
+  //   }
 });
 
 app.listen(PORT, () => {
--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const { default: axios } = require("axios");
 const app = express();
 const PORT = 5000;
 
@@ -14,13 +15,22 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post("http://localhost:7000/events", {
+    type: "CommentAdded",
+    data: { postId: req.params.id, commentId, content },
+  });
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body);
+  res.send({});
 });
 
 app.listen(PORT, () => {
